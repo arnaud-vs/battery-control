e3bb94728df87c9cd820096776e7de3985208fb5
Arnaud Verstraeten <arnaud.verstraeten@outlook.com>
2026-02-03 13:59:34 +0100

    Rule based control

--- DA_Optimization_Git/src/analysis/postprocessing.py
+++ DA_Optimization_Git/src/analysis/postprocessing.py
@@ -5,7 +5,7 @@ from ip_results import load_results_parquet
 from pathlib import Path
 
 # Plotting functions
-def plot_trading_decisions(results, labels, prefixes, forecast, real_prices, start, end):
+def plot_trading_decisions(results, labels, prefixes, forecast, real_prices, start, end, probabilistic, quantiles, alphas):
     forecast = forecast.loc[start:end]
     real_prices = real_prices.loc[start:end]
 
@@ -19,8 +19,13 @@ def plot_trading_decisions(results, labels, prefixes, forecast, real_prices, sta
         soc = df[f'{prefixes[i]}.E_start_kwh'] / battery['energy_kwh'] * 100
 
         axs[0, i].grid(alpha=0.5)
-        axs[0, i].step(real_prices.index, real_prices, where='post', label='Real imbalance price')
-        axs[0, i].step(forecast.index, forecast['q0.50_qh1'], where='post', label='Forecast QH1')
+        axs[0, i].step(real_prices.index, real_prices, where='post', label='Real imbalance price', color='C1')
+        if probabilistic[i]:
+            for q in range(len(quantiles)):
+                lower = forecast[f'q{quantiles[q]:.2f}_qh1']
+                upper = forecast[f'q{1 - quantiles[q]:.2f}_qh1']
+                axs[0, i].fill_between(forecast.index, lower, upper, step='post', color='C0', alpha=alphas[q])
+        axs[0, i].step(forecast.index, forecast['q0.50_qh1'], where='post', label='Forecast QH1', color='C0')
         axs[0, i].legend()
         axs[0, i].set_ylabel('Imbalance price [€/MWh]')
         axs[0, i].set_title(labels[i])
@@ -37,7 +42,7 @@ def plot_trading_decisions(results, labels, prefixes, forecast, real_prices, sta
         axs[2, i].grid(alpha=0.5)
         axs[2, i].plot(soc, label='Battery SoC', color='C0')
         axs[2, i].axhline(battery['soc_max']*100, linestyle='--', color='C0', label='SoC limits')
-        axs[2, i].axhline(-battery['soc_min']*100, linestyle='--', color='C0')
+        axs[2, i].axhline(battery['soc_min']*100, linestyle='--', color='C0')
         axs[2, i].legend()
         axs[2, i].set_ylabel('State of Charge [%]')
         axs[2, i].tick_params(labelleft=True)
@@ -51,6 +56,30 @@ def plot_trading_decisions(results, labels, prefixes, forecast, real_prices, sta
     fig.supxlabel('Time')
     plt.show()
 
+
+def plot_cumulative_results(results, labels, prefixes):
+    rbc = pd.read_csv('../../results/rbc/rbc.csv', index_col='datetime')
+    rbc.index = pd.to_datetime(rbc.index)
+
+    fig, axs = plt.subplots(1, 2, sharex='all')
+    for i, r in enumerate(results):
+        df, metadata = r
+
+        axs[0].plot(df[f'{prefixes[i]}.profit_realized_eur'].cumsum(), label=labels[i])
+        axs[0].plot(rbc['Profit'].cumsum(), label='Rule-based control')
+        axs[0].legend()
+        axs[0].set_ylabel('Cumulative profit [€]')
+        axs[0].set_title('Cumulative profit')
+
+        axs[1].plot((df[f'{prefixes[i]}.e_ch_kwh']+df[f'{prefixes[i]}.e_ch_kwh']).cumsum(), label=labels[i])
+        axs[1].plot(1000*rbc['Volume'].abs().cumsum(), label='Rule-based control')
+        axs[1].legend()
+        axs[1].set_ylabel('Cumulative traded volume [kWh]')
+        axs[1].set_title('Cumulative traded volume')
+    fig.supxlabel('Time')
+    plt.show()
+
+
 def plot_profit_as_function_of_labels(results, labels, prefixes):
     tot_profits = []
     for i, r in enumerate(results):
@@ -61,16 +90,44 @@ def plot_profit_as_function_of_labels(results, labels, prefixes):
     plt.ylabel('Profit [€]')
     plt.show()
 
+
+def plot_daily_profit_over_time(results, labels, prefixes):
+    for i, r in enumerate(results):
+        df, metadata = r
+        daily_profit = df[f'{prefixes[i]}.profit_realized_eur'].groupby(pd.Grouper(freq='W')).sum()
+        plt.plot(daily_profit, label=labels[i])
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Profit [€]')
+    plt.title('Profit per day')
+    plt.grid(alpha=0.5)
+    plt.show()
+
+
+def plot_trades_histogram(results, labels, prefixes):
+    fig, axs = plt.subplots(len(results), 1, sharex='all', sharey='all')
+    for i, r in enumerate(results):
+        df, metadata = r
+        daily_profit = df[f'{prefixes[i]}.profit_realized_eur'].groupby(pd.Grouper(freq='H')).sum()
+        axs[i].hist(x=df[f'{prefixes[i]}.profit_realized_eur']) #, bins=range(-1000, 2000, 100))
+        axs[i].grid(alpha=0.5)
+        axs[i].set_title(labels[i])
+    fig.supxlabel('Daily profit [€]')
+    fig.supylabel('Days [-]')
+    fig.suptitle('Daily profit histogram')
+    plt.show()
+
+
 # Specify paths to compare, and their labels for plotting
 paths_to_compare = [
-                    'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L1_0.01_CP_0__v1.parquet',
+                    # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L1_0.01_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L1_0.03_CP_0__v1.parquet',
-                    # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L1_0.1_CP_0__v1.parquet',
+                    'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L1_0.1_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L1_0.3_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.001_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.003_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.01_CP_0__v1.parquet',
-                    'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.03_CP_0__v1.parquet',
+                    # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.03_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.1_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.01_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.3_CP_0__v1.parquet',
@@ -79,15 +136,21 @@ paths_to_compare = [
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L2_0.001_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L2_0.03_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L2_0.003_CP_0__v1.parquet'
-                    # 'ip_rolling_prob/ip_rolling_prob_qr_quantile_paths_cvar_a0p95_l0p1_TP_L1_0.1_CP_0_v1.parquet'
+                    'ip_rolling_prob/ip_rolling_prob_qr_quantile_paths_cvar_a0p95_l0p1_TP_L1_0.1_CP_0_v1.parquet'
                     # 'ip_rolling_prob/ip_rolling_prob_qr_quantile_paths_cvar_a0p95_l0p3_TP_L1_0.1_CP_0_v1.parquet'
                     ]
+
 # labels = ['QR forecast', 'Perfect foresight']
-labels = ['L1 0.01', 'L2 0.01']
+# labels = ['L1 0.01', 'L2 0.01']
+# labels = ['Deterministic forecast', 'Perfect foresight', 'Probabilistic forecast']
+labels = ['Deterministic forecast', 'Probabilistic forecast']
 # labels = ['L1\n0.01', 'L1\n0.03', 'L1\n0.1', 'L1\n0.3', 'L2\n0.001', 'L2\n0.003', 'L2\n0.01', 'L2\n0.03']
 # prefixes = ['qr.forecast', 'benchmark.perfect_foresight']
-prefixes = ['qr.forecast']*2
+# prefixes = ['qr.forecast']*2
+# prefixes = ['qr.forecast', 'benchmark.perfect_foresight', 'qr.prob.cvar_a0p95_l0p1']
+prefixes = ['qr.forecast', 'qr.prob.cvar_a0p95_l0p1']
 # prefixes = ['qr.forecast']*8
+probabilistic = [0, 1]
 forecast_path = 'IP_QR.csv'
 
 # Load the desired results
@@ -112,8 +175,16 @@ forecast.index = pd.to_datetime(forecast.index)
 real_prices.index = pd.to_datetime(real_prices.index)
 
 # Make your favorite plots
-start = pd.Timestamp(year=2023, month=6, day=1, hour=18)
-end = pd.Timestamp(year=2023, month=6, day=2, hour=0)
-plot_trading_decisions(results, labels, prefixes, forecast, real_prices, start, end)
+start = pd.Timestamp(year=2023, month=1, day=25, hour=16)
+end = pd.Timestamp(year=2023, month=1, day=25, hour=22)
+quantiles = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4]
+alphas = [0.01, 0.05, 0.1, 0.2, 0.4, 0.6]
+# plot_trading_decisions(results, labels, prefixes, forecast, real_prices, start, end, probabilistic, quantiles, alphas)
+
+plot_cumulative_results(results, labels, prefixes)
 
 # plot_profit_as_function_of_labels(results, labels, prefixes)
+
+# plot_daily_profit_over_time(results, labels, prefixes)
+
+# plot_trades_histogram(results, labels, prefixes)
--- rbc.py
+++ rbc.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./Data/data_ip_min_23.csv', index_col='datetime')
-df.index = pd.to_datetime(df.index)
-ip_min = df['ip']
 df_qh = pd.read_csv('./Data/data_ip_qh_23.csv', index_col='datetime')
 df_qh.index = pd.to_datetime(df_qh.index)
 ip_qh = df_qh['ip']
@@ -13,27 +10,30 @@ P = 4
 quantile_window = pd.Timedelta(days=3*30)
 start = pd.Timestamp(year=2023, month=1, day=1, hour=0, tz='utc')
 end = pd.Timestamp(year=2023, month=12, day=31, hour=23, tz='utc')
-ip_min = ip_min.loc[start:end]
+ip_qh = ip_qh.loc[start:end]
 
 # actions: (SoC lower, SoC upper, price quantile lower, price quantile upper, action)
 charge, wait, discharge = 1, 0, -1
-actions = [(0,  5,   0,    1,    charge),
-           (5,  30,  0,    0.4,  charge),
-           (30, 60,  0,    0.25, charge),
-           (60, 95,  0,    0.1,  charge),
-           (5,  40,  0.9,  1,    discharge),
-           (40, 70,  0.8,  1,    discharge),
-           (70, 95,  0.7,  1,    discharge),
-           (95, 100, 0,    1,    discharge)]
+# actions = [(0,  5,   0,    1,    charge),
+#            (5,  30,  0,    0.4,  charge),
+#            (30, 60,  0,    0.25, charge),
+#            (60, 95,  0,    0.1,  charge),
+#            (5,  40,  0.9,  1,    discharge),
+#            (40, 70,  0.8,  1,    discharge),
+#            (70, 95,  0.7,  1,    discharge),
+#            (95, 100, 0,    1,    discharge)]
+actions = [(10, 15, 0,   1,   charge),
+           (15, 85, 0,   0.5, charge),
+           (15, 85, 0.5, 1,   discharge),
+           (85, 90, 0,   1,   discharge)]
 
-state = pd.DataFrame(index=ip_min.index, columns=['SoC', 'Profit', 'Volume', 'Q'])
+state = pd.DataFrame(index=ip_qh.index, columns=['SoC', 'Profit', 'Volume', 'Q'])
 state['month'] = state.index.month
 soc = 50
 month = start.month
-for min in state.index:
-    qh = min.floor('15min')
-    ip_min_window = ip_qh.loc[qh - quantile_window:qh]
-    q = sum(ip_min_window <= ip_min.loc[min]) / len(ip_min_window)
+for qh in state.index:
+    ip_qh_window = ip_qh.loc[qh - quantile_window:qh]
+    q = sum(ip_qh_window <= ip_qh.loc[qh]) / len(ip_qh_window)
 
     decision = wait
     for a in actions:
@@ -41,19 +41,21 @@ for min in state.index:
             if (q >= a[2]) & (q < a[3]):
                 decision = a[4]
 
-    volume = decision*P/60
+    volume = decision*P/4
     profit = -ip_qh.loc[qh]*volume
     soc_mwh = soc*E/100 + volume
     soc = soc_mwh*100 / E
 
-    state.at[min, 'Q'] = q
-    state.at[min, 'Volume'] = volume
-    state.at[min, 'Profit'] = profit
-    state.at[min, 'SoC'] = soc
+    state.at[qh, 'Q'] = q
+    state.at[qh, 'Volume'] = volume
+    state.at[qh, 'Profit'] = profit
+    state.at[qh, 'SoC'] = soc
 
-    if min.month != month:
+    if qh.month != month:
         print(f'Month {month} done')
-        month = min.month
+        month = qh.month
+
+state.to_csv('./DA_Optimization_Git/results/rbc/rbc.csv')
 
 cycle = 2*E
 n_days = len(state) / (24*60)
