58f7b0ea113fd4e5f6e4f6accdfe8120c01da8e6
Arnaud Verstraeten <arnaud.verstraeten@outlook.com>
2026-02-04 08:28:30 +0100

    Copula results

--- DA_Optimization_Git/src/analysis/postprocessing.py
+++ DA_Optimization_Git/src/analysis/postprocessing.py
@@ -25,7 +25,8 @@ def plot_trading_decisions(results, labels, prefixes, forecast, real_prices, sta
                 lower = forecast[f'q{quantiles[q]:.2f}_qh1']
                 upper = forecast[f'q{1 - quantiles[q]:.2f}_qh1']
                 axs[0, i].fill_between(forecast.index, lower, upper, step='post', color='C0', alpha=alphas[q])
-        axs[0, i].step(forecast.index, forecast['q0.50_qh1'], where='post', label='Forecast QH1', color='C0')
+        if prefixes[i] != 'benchmark.perfect_foresight':
+            axs[0, i].step(forecast.index, forecast['q0.50_qh1'], where='post', label='Forecast QH1', color='C0')
         axs[0, i].legend()
         axs[0, i].set_ylabel('Imbalance price [€/MWh]')
         axs[0, i].set_title(labels[i])
@@ -58,24 +59,26 @@ def plot_trading_decisions(results, labels, prefixes, forecast, real_prices, sta
 
 
 def plot_cumulative_results(results, labels, prefixes):
-    rbc = pd.read_csv('../../results/rbc/rbc.csv', index_col='datetime')
-    rbc.index = pd.to_datetime(rbc.index)
+    # rbc = pd.read_csv('../../results/rbc/rbc.csv', index_col='datetime')
+    # rbc.index = pd.to_datetime(rbc.index)
 
     fig, axs = plt.subplots(1, 2, sharex='all')
     for i, r in enumerate(results):
         df, metadata = r
 
         axs[0].plot(df[f'{prefixes[i]}.profit_realized_eur'].cumsum(), label=labels[i])
-        axs[0].plot(rbc['Profit'].cumsum(), label='Rule-based control')
+        # axs[0].plot(rbc['Profit'].cumsum(), label='Rule-based control')
         axs[0].legend()
         axs[0].set_ylabel('Cumulative profit [€]')
         axs[0].set_title('Cumulative profit')
+        axs[0].grid(alpha=0.5)
 
         axs[1].plot((df[f'{prefixes[i]}.e_ch_kwh']+df[f'{prefixes[i]}.e_ch_kwh']).cumsum(), label=labels[i])
-        axs[1].plot(1000*rbc['Volume'].abs().cumsum(), label='Rule-based control')
+        # axs[1].plot(1000*rbc['Volume'].abs().cumsum(), label='Rule-based control')
         axs[1].legend()
         axs[1].set_ylabel('Cumulative traded volume [kWh]')
         axs[1].set_title('Cumulative traded volume')
+        axs[1].grid(alpha=0.5)
     fig.supxlabel('Time')
     plt.show()
 
@@ -108,13 +111,14 @@ def plot_trades_histogram(results, labels, prefixes):
     fig, axs = plt.subplots(len(results), 1, sharex='all', sharey='all')
     for i, r in enumerate(results):
         df, metadata = r
-        daily_profit = df[f'{prefixes[i]}.profit_realized_eur'].groupby(pd.Grouper(freq='H')).sum()
-        axs[i].hist(x=df[f'{prefixes[i]}.profit_realized_eur']) #, bins=range(-1000, 2000, 100))
+        # daily_profit = df[f'{prefixes[i]}.profit_realized_eur'].groupby(pd.Grouper(freq='H')).sum()
+        axs[i].hist(x=df[f'{prefixes[i]}.profit_realized_eur'], bins=range(-1000, 2000, 50))
         axs[i].grid(alpha=0.5)
         axs[i].set_title(labels[i])
-    fig.supxlabel('Daily profit [€]')
-    fig.supylabel('Days [-]')
-    fig.suptitle('Daily profit histogram')
+        axs[i].set_yscale('log')
+    fig.supxlabel('Trading profit [€]')
+    fig.supylabel('Trades [-]')
+    fig.suptitle('Trading profit histogram')
     plt.show()
 
 
@@ -128,7 +132,7 @@ paths_to_compare = [
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.003_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.01_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_forecast_TP_L2_0.03_CP_0__v1.parquet',
-                    # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.1_CP_0__v1.parquet',
+                    'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.1_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.01_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.3_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L1_0.03_CP_0__v1.parquet',
@@ -136,21 +140,23 @@ paths_to_compare = [
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L2_0.001_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L2_0.03_CP_0__v1.parquet',
                     # 'ip_rolling_ce/ip_rolling_ce_qr_deterministic_perfect_foresight_TP_L2_0.003_CP_0__v1.parquet'
-                    'ip_rolling_prob/ip_rolling_prob_qr_quantile_paths_cvar_a0p95_l0p1_TP_L1_0.1_CP_0_v1.parquet'
+                    # 'ip_rolling_prob/ip_rolling_prob_qr_quantile_paths_cvar_a0p95_l0p1_TP_L1_0.1_CP_0_v1.parquet'
                     # 'ip_rolling_prob/ip_rolling_prob_qr_quantile_paths_cvar_a0p95_l0p3_TP_L1_0.1_CP_0_v1.parquet'
+                    # 'ip_rolling_prob/ip_rolling_prob_qr_copula_lambCorr_0.995_cvar_a0p95_l0p1_TP_L1_0.1_CP_0_v1.parquet'
+                    'ip_rolling_prob/ip_rolling_prob_qr_copula_lambCorr_0.995_cvar_a0p95_l0p3_TP_L1_0.1_CP_0_v1.parquet'
                     ]
 
 # labels = ['QR forecast', 'Perfect foresight']
 # labels = ['L1 0.01', 'L2 0.01']
-# labels = ['Deterministic forecast', 'Perfect foresight', 'Probabilistic forecast']
-labels = ['Deterministic forecast', 'Probabilistic forecast']
+labels = ['Deterministic forecast', 'Perfect foresight', 'Probabilistic forecast']
+# labels = ['Deterministic forecast', 'Probabilistic forecast']
 # labels = ['L1\n0.01', 'L1\n0.03', 'L1\n0.1', 'L1\n0.3', 'L2\n0.001', 'L2\n0.003', 'L2\n0.01', 'L2\n0.03']
 # prefixes = ['qr.forecast', 'benchmark.perfect_foresight']
 # prefixes = ['qr.forecast']*2
-# prefixes = ['qr.forecast', 'benchmark.perfect_foresight', 'qr.prob.cvar_a0p95_l0p1']
-prefixes = ['qr.forecast', 'qr.prob.cvar_a0p95_l0p1']
+prefixes = ['qr.forecast', 'benchmark.perfect_foresight', 'qr.prob.cvar_a0p95_l0p3']
+# prefixes = ['qr.forecast', 'qr.prob.cvar_a0p95_l0p1']
 # prefixes = ['qr.forecast']*8
-probabilistic = [0, 1]
+probabilistic = [0, 0, 1]
 forecast_path = 'IP_QR.csv'
 
 # Load the desired results
@@ -175,11 +181,13 @@ forecast.index = pd.to_datetime(forecast.index)
 real_prices.index = pd.to_datetime(real_prices.index)
 
 # Make your favorite plots
-start = pd.Timestamp(year=2023, month=1, day=25, hour=16)
-end = pd.Timestamp(year=2023, month=1, day=25, hour=22)
+# start = pd.Timestamp(year=2023, month=1, day=25, hour=16)
+# end = pd.Timestamp(year=2023, month=1, day=25, hour=22)
+start = pd.Timestamp(year=2023, month=1, day=1, hour=0)
+end = pd.Timestamp(year=2023, month=1, day=1, hour=6)
 quantiles = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4]
 alphas = [0.01, 0.05, 0.1, 0.2, 0.4, 0.6]
-# plot_trading_decisions(results, labels, prefixes, forecast, real_prices, start, end, probabilistic, quantiles, alphas)
+plot_trading_decisions(results, labels, prefixes, forecast, real_prices, start, end, probabilistic, quantiles, alphas)
 
 plot_cumulative_results(results, labels, prefixes)
 
@@ -187,4 +195,4 @@ plot_cumulative_results(results, labels, prefixes)
 
 # plot_daily_profit_over_time(results, labels, prefixes)
 
-# plot_trades_histogram(results, labels, prefixes)
+plot_trades_histogram(results, labels, prefixes)
